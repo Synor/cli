db2f0cc6b6dfd14bbd7318036b19ef80346e87e2
MunifTanjim <muniftanjim@gmail.com>
2019-12-13 23:11:11 +0600

    Add src/commands/repair

---
+++ src/commands/repair.ts
@@ -0,0 +1,38 @@
+import { cli } from 'cli-ux'
+import Command from '../command'
+
+export default class Repair extends Command {
+  static description = 'repair mismatched hashes and delete dirty records'
+
+  static examples = [`$ synor repair`]
+
+  static flags = {
+    ...Command.flags
+  }
+
+  static args = []
+
+  async run() {
+    const { migrator } = this.synor
+
+    migrator
+      .on('repair:start', () => {
+        cli.action.start('Repairing')
+      })
+      .on('repair:end', () => {
+        cli.action.stop('done!')
+      })
+
+    await migrator.open()
+
+    const confirmed = await cli.confirm('Are you sure?! (y/n)')
+
+    if (confirmed) {
+      await migrator.repair()
+    } else {
+      this.log('Skipping repair...')
+    }
+
+    await migrator.close()
+  }
+}

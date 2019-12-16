e3ba9ccb229a596c098c15b441bb4769ac370cda
MunifTanjim <muniftanjim@gmail.com>
2019-12-16 16:11:35 +0600

    Tweak src/commands/migrate

--- src/commands/migrate.ts
+++ src/commands/migrate.ts
@@ -1,9 +1,8 @@
 import { cli } from 'cli-ux'
 import Command from '../command'
-import { isSynorError, isValidationErrorType } from '../utils/error'
 
 export default class Migrate extends Command {
-  static description = 'run migrations'
+  static description = 'migrate to targetVersion'
 
   static examples = [`$ synor migrate`]
 
@@ -29,14 +28,12 @@ export default class Migrate extends Command {
     migrator
       .on('current', record => {
         currentVersion = record.version
-        this.log(`Current Version: ${currentVersion!}`)
-        this.log(`Target Version: ${args.targetVersion}`)
       })
       .on('migrate:start', () => {
-        this.log(`Starting migration...`)
+        this.debug('Starting migration...')
       })
       .on('migrate:end', () => {
-        this.log('Migration done!')
+        this.debug('Finished migration!')
       })
       .on('migrate:run:start', source => {
         cli.action.start(
@@ -46,25 +43,16 @@ export default class Migrate extends Command {
       .on('migrate:run:end', () => {
         cli.action.stop('done!')
       })
-      .on('validate:error', (_record, error) => {
-        if (isSynorError(error) && isValidationErrorType(error.type)) {
-          this.error(
-            [
-              `Validation Error (${error.type}) =>`,
-              `Version(${error.data.version})`,
-              `Type(${error.data.type})`,
-              error.data.title && `Title(${error.data.title})`,
-              `\nRun validation for more information:`,
-              `\n$ ${this.config.bin} validate`
-            ].join(' '),
-            { code: error.type, exit: 1 }
-          )
-        }
-      })
 
     await migrator.current()
 
-    const confirmed = await cli.confirm('Continue? (y/n)')
+    const confirmed = await cli.confirm(
+      [
+        `Current Version: ${currentVersion!}`,
+        `Target Version: ${args.targetVersion}`,
+        `Continue? (y/n)`
+      ].join('\n')
+    )
 
     if (confirmed) {
       await migrator.validate()
